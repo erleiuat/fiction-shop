03db932d33fd6e3bbf474dac295a913e463ff438
Elia Reutlinger <reutlinger.elia@gmail.com>
2021-08-15 12:09:35 +0200

    sdffds

--- public/shop_data/items/index.js
+++ public/shop_data/items/index.js
@@ -3,13 +3,15 @@ import premium from 'public/shop_data/items/premium.json'
 import vehicles from 'public/shop_data/items/vehicles.json'
 import clothing from 'public/shop_data/items/clothing.json'
 import tools from 'public/shop_data/items/tools.json'
+import components from 'public/shop_data/items/components.json'
 import food from 'public/shop_data/items/food.json'
 import pharmacy from 'public/shop_data/items/pharmacy.json'
 
 function addDiscount(item) {
   item.price_fame = Math.round(item.price_fame * 1.4)
   item.price = item.price_fame + '.00'
-  return item
+  item.buy_command = '!buyitem ' + item.key
+  if (item.keyword) return item
 }
 
 module.exports = [
@@ -18,6 +20,7 @@ module.exports = [
   ...vehicles.map(e => addDiscount(e)),
   ...clothing.map(e => addDiscount(e)),
   ...tools.map(e => addDiscount(e)),
+  ...components.map(e => addDiscount(e)),
   ...food.map(e => addDiscount(e)),
   ...pharmacy.map(e => addDiscount(e))
 ]
